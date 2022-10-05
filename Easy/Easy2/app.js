4256dc247e6c23a67acd974ac9dcb5668954f51d
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-10-05 13:50:15 -0400

    Update app.js

--- Easy/Easy2/app.js
+++ Easy/Easy2/app.js
@@ -1,1 +1,16 @@
-
+function sort(numbers) {
+  var prime = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      even.push(numbers[i]);
+    } else {
+      odd.push(numbers[i]);
+    }
+  }
+  const returnObject = {
+    odd,
+    even,
+  };
+  return returnObject;
+}
+console.log(sort([2, 4, 7, 11, 15, 16]));

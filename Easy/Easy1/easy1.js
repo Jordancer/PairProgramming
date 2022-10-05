a1d7b03f565264b580ab7810fe6dc5d0cac09b47
Jordan Dancer <112099523+Jordancer@users.noreply.github.com>
2022-10-05 11:46:26 -0400

    Easy 1 Complete

---
+++ Easy/Easy1/easy1.js
@@ -0,0 +1,19 @@
+function sort(numbers) {
+  var odd = [];
+  var even = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      even.push(numbers[i]);
+    } else {
+      odd.push(numbers[i]);
+    }
+  }
+  const returnObject = {
+    odd,
+    even,
+  };
+  return returnObject;
+}
+console.log(sort([2, 4, 7, 11, 15, 16]));
+
+//-----------------------------------------
